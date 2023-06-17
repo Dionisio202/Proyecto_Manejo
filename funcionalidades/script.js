954ca2c0d6554f306b54ed8d1805c6a2f868eb68
Edison <solisedison@outlook.com>
2023-06-17 15:42:54 -0500

    Se agrego el fromulario para que sea de tipo modal

--- funcionalidades/script.js
+++ funcionalidades/script.js
@@ -1,3 +1,40 @@
-document.getElementById("btnAgregar").addEventListener("click", function() {
-    document.getElementById("formulario").style.display = "block";
-});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var btnAgregar = document.getElementById("btnAgregar");
+    var formularioModal = document.getElementById("formularioModal");
+  
+   
+    btnAgregar.addEventListener("click", function() {
+     
+      var bootstrapModal = new bootstrap.Modal(formularioModal);
+      bootstrapModal.show();
+    });
+  
+  
+    var formulario = document.getElementById("formulario");
+    var nombreTareaInput = document.getElementById("nombreTarea");
+    var fechaInicioInput = document.getElementById("fechaInicio");
+    var fechaFinalInput = document.getElementById("fechaFinal");
+    var descripcionTextarea = document.getElementById("descripcion");
+  
+   
+    formulario.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+      // Este es el codigo para cuando se da guardar 
+      var nombreTarea = nombreTareaInput.value;
+      var fechaInicio = fechaInicioInput.value;
+      var fechaFinal = fechaFinalInput.value;
+      var descripcion = descripcionTextarea.value;
+  
+    
+      nombreTareaInput.value = "";
+      fechaInicioInput.value = "";
+      fechaFinalInput.value = "";
+      descripcionTextarea.value = "";
+  
+      var bootstrapModal = bootstrap.Modal.getInstance(formularioModal);
+      bootstrapModal.hide();
+    });
+  });
+  
