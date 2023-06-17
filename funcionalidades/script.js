b3b2233337585117ffb3e855bb32db7b86bc5133
Edison <solisedison@outlook.com>
2023-06-17 13:56:28 -0500

    Se agrego el foormulario al final de las tareas hechas

--- funcionalidades/script.js
+++ funcionalidades/script.js
@@ -1,13 +1,3 @@
-
-const contenedorFormulario = document.getElementById('btnAgregar');
-
-botonAgregar.addEventListener('click', function() {
-  
-    const formulario = document.createElement('form');
-
-    // Agregar campos y elementos al formulario
-    // ...
-
-    // Agregar el formulario al contenedor
-    contenedorFormulario.appendChild(formulario);
+document.getElementById("btnAgregar").addEventListener("click", function() {
+    document.getElementById("formulario").style.display = "block";
 });
