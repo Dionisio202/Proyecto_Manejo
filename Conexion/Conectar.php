49a4cedf575c9220cf9884aa6eed89058ccb8724
Marlon Masabanda <mmasabanda5946@uta.edu.ec>
2023-06-19 17:17:50 -0500

    Se agrego el archivo .gitignore y la carpeta de Conexion que contiene un archivo php Conectar.php que logra la comunicacion entre el formulario y la BD

---
+++ Conexion/Conectar.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ID_TAREA = NULL;
+    $NOM_TAREA = $_POST['nombreTarea'];
+    $FEC_INI = $_POST['fechaInicio'];
+    $FEC_FIN = $_POST['fechaFinal'];
+    $DESCRIPCION = $_POST['descripcion'];
+    $ESTADO = 'PHA';
+
+    $dsn = 'mysql:host=localhost;dbname=proyecto_manejo';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $conexion = new PDO($dsn, $username, $password);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = 'INSERT INTO TAREAS VALUES (?,?,?,?,?,?)';
+        $statement = $conexion->prepare($sql);
+        $statement ->bindParam(1,$ID_TAREA);
+        $statement ->bindParam(2,$NOM_TAREA);
+        $statement ->bindParam(3,$FEC_INI);
+        $statement ->bindParam(4,$FEC_FIN);
+        $statement ->bindParam(5,$DESCRIPCION);
+        $statement ->bindParam(6,$ESTADO);
+        $statement->execute();
+        exit();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+
+
