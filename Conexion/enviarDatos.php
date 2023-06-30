a92a16ff2a7c8f5cc06fa870150443f1688b0a32
Marlon Masabanda <mmasabanda5946@uta.edu.ec>
2023-06-29 21:22:18 -0500

    Se agrego el archivo resivirDatos.php que trae todos los datos de la BD y los prepara en un Json para usarlos en un JS

--- Conexion/enviarDatos.php
+++ Conexion/enviarDatos.php
@@ -6,28 +6,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $FEC_FIN = $_POST['fechaFinal'];
     $DESCRIPCION = $_POST['descripcion'];
     $ESTADO = 'PHA';
-
     $dsn = 'mysql:host=localhost;dbname=proyecto_manejo';
     $username = 'root';
     $password = '';
-
     try {
         $conexion = new PDO($dsn, $username, $password);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = 'INSERT INTO TAREAS VALUES (?,?,?,?,?,?)';
         $statement = $conexion->prepare($sql);
-        $statement ->bindParam(1,$ID_TAREA);
-        $statement ->bindParam(2,$NOM_TAREA);
-        $statement ->bindParam(3,$FEC_INI);
-        $statement ->bindParam(4,$FEC_FIN);
-        $statement ->bindParam(5,$DESCRIPCION);
-        $statement ->bindParam(6,$ESTADO);
+        $statement->bindParam(1, $ID_TAREA);
+        $statement->bindParam(2, $NOM_TAREA);
+        $statement->bindParam(3, $FEC_INI);
+        $statement->bindParam(4, $FEC_FIN);
+        $statement->bindParam(5, $DESCRIPCION);
+        $statement->bindParam(6, $ESTADO);
         $statement->execute();
+        echo "<script>window.location.href = '../index.html';</script>";
         exit();
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
 }
 ?>
-
-
---
+++ Conexion/resivirDatos.php
@@ -0,0 +1,21 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=proyecto_manejo';
+$username = 'root';
+$password = '';
+try {
+    $conexion = new PDO($dsn, $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = 'SELECT * FROM TAREAS';
+    $statement = $conexion->prepare($sql);
+    $statement->execute();
+    $resultados = array();
+    while ($fila = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $resultados[] = $fila;
+    }
+    $datosJson = json_encode($resultados);
+    echo $datosJson;
+    exit();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
