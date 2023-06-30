503068957ca71cb3f4da9ab835e4c01c5c5dda1a
William <wulli.mu28@gmail.com>
2023-06-30 00:20:03 -0500

    creacion de funcionalidad: Las tareas se recuperan directamente de la base de datos y se agregan a la seccion 'Por hacer'

--- funcionalidades/script.js
+++ funcionalidades/script.js
@@ -1,19 +1,88 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var btnAgregar = document.getElementById("btnAgregar");
-  var formularioModal = document.getElementById("formularioModal");
+  
+  
+  //ENCONTRAMOS EL BOTON PARA QUE APAREZCA EL FORMULARIO PARA AGREGAR UNA TAREA
+  const btnAgregar = document.getElementById("btnAgregar");
 
+  //ENCONTRAMOS EL FORMULARIO PARA AGREGAR UNA TAREA
+  const formularioModal = document.getElementById("formularioModal");
+  
+  const formulario = document.getElementById("formulario1");
+  //ENCONTRAMOS EL CONTENEDOR DE LAS TARJETAS POR HACER
+  const contenedorTareasPorHacer = document.getElementById("contenedorTareasPorHacer");
+  //ENCONTRAMOS EL CONTENEDOR DE LAS TARJETAS EN PROCESO
+  const contenedorTareasEnProceso = document.getElementById("contenedorTareasEnProceso");
+  //ENCONTRAMOS EL CONTENEDOR DE LAS TARJETAS TERMINADAS
+  const contenedorTareasTerminadas = document.getElementById("contenedorTareasHechas");
+  
+  //DEFINIMOS UNA VARIABLE QUE CONTENDRA LOS DATOS RECUPERADOS DEL ARCHIVO PHP
+  var Datos = [];
+  
+  recuperarDatos();
+
+
+  //EVENTO QUE AL APLASTARLO MUESTRA EL FORMULARIO PARA AGREGAR UNA TAREA
   btnAgregar.addEventListener("click", function() {
     var bootstrapModal = new bootstrap.Modal(formularioModal);
     bootstrapModal.show();
   });
 
-  var formulario = document.getElementById("formulario");
-  var nombreTareaInput = document.getElementById("nombreTarea");
-  var fechaInicioInput = document.getElementById("fechaInicio");
-  var fechaFinalInput = document.getElementById("fechaFinal");
-  var descripcionTextarea = document.getElementById("descripcion");
+  mostrar();
 
-  formulario.addEventListener("submit", function(event) {
+
+  //METODO PARA RECUPERAR LOS DATOS DEL ARCHIVO PHP
+  function recuperarDatos() {
+    axios.get('../Conexion/RecibirDatos.php')
+    .then(response => {
+      var datos = response.data; 
+      UbicarTareas(datos);
+    })
+    .catch(error => console.log(error + " AQUI ES EL ERROR"));
+  }
+  
+  
+  //METODO PARA UBICAR LAS TAREAS DENTRO DEL CONTENEDOR. ADEMÁS, GUARDAMOS LAS TAREAS 
+  //EN LA VARIABLE DATOS PARA TENER ACCESO MÁS ADELANTE
+  function UbicarTareas(datos) {
+    
+    Datos = datos;
+    datos.forEach(element => {
+      console.log(element.ID_TAREA);
+      var nuevaTarjeta = crearTarea(element.ID_TAREA,element.NOM_TAREA);
+      contenedorTareasPorHacer.appendChild(nuevaTarjeta);
+      
+    });
+  }
+
+  
+ //METODO PARA CREAR LAS TAREAS 
+function crearTarea(id, nombreTarea){
+  
+  var nuevaTarjetaCont = document.createElement("div");
+  nuevaTarjetaCont.classList.add("card", "carta");
+  nuevaTarjetaCont.setAttribute("id", "Tarea-"+id);
+
+  nuevaTarjetaCont.innerHTML =  ` 
+  <div class="card-header d-flex justify-content-between align-items-center">
+  <h3 class="card-title small">`+nombreTarea+`</h3>
+  <div class="card-icons">
+  <div class="d-flex">
+  <button class="btn btn-success mr-2 rounded-circle">
+  <span><i class="bi bi-check"></i></span>
+  </button>
+  <button class="btn btn-danger rounded-circle ms-2">
+  <span><i class="bi bi-trash"></i></span>
+  </button>
+  </div>
+  </div>
+  </div>
+  `;
+  
+  return nuevaTarjetaCont;
+}
+
+  
+  formulario.addEventListener("s", function(event) {
     event.preventDefault();
 
     var nombreTarea = nombreTareaInput.value;
@@ -26,13 +95,13 @@ document.addEventListener("DOMContentLoaded", function() {
     fechaFinalInput.value = "";
     descripcionTextarea.value = "";
 
-    var nuevaTarjeta = crearTarea(nombreTarea);
+    //var nuevaTarjeta = crearTarea(nombreTarea);
 
 
-    var contenedorTarjetas = document.getElementById("contenedorTarjetas");
-    contenedorTarjetas.appendChild(nuevaTarjeta);
+    //var contenedorTarjetas = document.getElementById("contenedorTarjetas");
+    //contenedorTarjetas.appendChild(nuevaTarjeta);
 
-    var bootstrapModal = bootstrap.Modal.getInstance(formularioModal);
+    //var bootstrapModal = bootstrap.Modal.getInstance(formularioModal);
     bootstrapModal.hide();
   });
 
@@ -71,31 +140,15 @@ cartas.forEach(function(carta) {
   });
 });
 
-});
 
 
 
 
-function crearTarea(nombreTarea){
 
-  var nuevaTarjetaCont = document.createElement("div");
-  nuevaTarjetaCont.classList.add("card", "carta");
 
-  nuevaTarjetaCont.innerHTML =  ` 
-  <div class="card-header d-flex justify-content-between align-items-center">
-    <h3 class="card-title small">Título de la tarea</h3>
-    <div class="card-icons">
-      <div class="d-flex">
-        <button class="btn btn-success mr-2 rounded-circle">
-          <span><i class="bi bi-check"></i></span>
-        </button>
-        <button class="btn btn-danger rounded-circle ms-2">
-          <span><i class="bi bi-trash"></i></span>
-        </button>
-      </div>
-    </div>
-  </div>
-`;
-
-  return nuevaTarjetaCont;
+function mostrar(){
+  console.log(Datos +"   Soy DATOS");
 }
+
+
+});
